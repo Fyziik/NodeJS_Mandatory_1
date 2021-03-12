dbb3868bc46ef46cba4e069f038805eea51937b8
Andreas Møller Laursen <Fyziikme@gmail.com>
2021-03-12 14:24:24 +0100

    Optimized backend code, making use of a local db instead of hitting an online mongoDB on each query

--- app.js
+++ app.js
@@ -17,8 +17,21 @@ app.use(cors())
 app.set('view engine', 'ejs')
 const port = 8080
 
+// mongoDB databased stored locally so I dont have to connect to the DB each time spending precious ressources
+// this happens on startup and is then saved locally in the 'pagesDatabase' variable, then updated appropiately during runtime
+let pagesDatabase
+mongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    const dbo = db.db("mandatoryDB");
+    dbo.collection("pages").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      pagesDatabase = result
+      db.close();
+    });
+});
 
 //For changing between light and dark mode
+//OPTIMIZED
 app.get("/changeTheme", (req, res) => {
     lightmode = !lightmode
     if (lightmode) {
@@ -26,71 +39,36 @@ app.get("/changeTheme", (req, res) => {
     } else {
         themeButtonText = "Lightmode"
     }
-    mongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        const dbo = db.db("mandatoryDB");
-        dbo.collection("pages").find({}).toArray(function(err, result) {
-          if (err) throw err;
-          db.close();
-
-          res.render('pages/index', {result: result, mode: lightmode, themeButtonText: themeButtonText})
-
-        });
-      });
-    
+    res.render('pages/index', {result: pagesDatabase, mode: lightmode, themeButtonText: themeButtonText})
 })
 
 //Index page serving
+//OPTIMIZED
 app.get("/", (req, res) => {
-    mongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        const dbo = db.db("mandatoryDB");
-        dbo.collection("pages").find({}).toArray(function(err, result) {
-          if (err) throw err;
-          db.close();
-
-          res.render('pages/index', {result: result, mode: lightmode, themeButtonText: themeButtonText})
-
-        });
-      });
-    
+    res.render('pages/index', {result: pagesDatabase, mode: lightmode, themeButtonText: themeButtonText})
 })
 
 //Session page serving from various IDs
+//This method will look through the pagesDatabase variable (holding all of the pages) and serve both the entire array of pages AND the specific page
+//OPTIMIZED
 app.get("/session/:title", (req, res) => {
     const title = req.params.title
-    mongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        const dbo = db.db("mandatoryDB");
-        dbo.collection("pages").find({title: title}).toArray(function(err, result) {
-            if (err) throw err;
-            dbo.collection("pages").find({}).toArray(function(err, allResults) {
-                if (err) throw err;
-                db.close();
+    const allResults = pagesDatabase
+    const result = pagesDatabase.filter(element => element.title === title)
 
-                res.render('pages/session', {result: allResults, pageContent: result, mode: lightmode, themeButtonText: themeButtonText})
-            })
-        });
-      });
+    // allResults = all pages, result = single page looking for, mode = light or dark mode, themeButtonText = text on button depending on state
+    res.render('pages/session', {result: allResults, pageContent: result, mode: lightmode, themeButtonText: themeButtonText})
     
 })
 
 //Page for new page creation
+//OPTIMIZED
 app.get("/addNewPage", (req, res) => {
-    mongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        const dbo = db.db("mandatoryDB");
-        dbo.collection("pages").find({}).toArray(function(err, result) {
-          if (err) throw err;
-          db.close();
-
-          res.render('pages/sessionAdd', {result: result, mode: lightmode, themeButtonText: themeButtonText})
-
-        });
-      });
+    res.render('pages/sessionAdd', {result: pagesDatabase, mode: lightmode, themeButtonText: themeButtonText})
 })
 
 //POST for adding a new page
+//OPTIMIZED
 //TODO Make title property in DB unique, so there wont be duplicate sites
 app.post("/newPage", (req, res) => {
     if (req.body.pageTitle !== "" && req.body.pageContent !== "") {
@@ -108,11 +86,14 @@ app.post("/newPage", (req, res) => {
                 if (err) res.redirect('/');
                 db.close;
             });
+            //If no errors with mongoDB, insert into local db
+            pagesDatabase.push(myObj)
         });
     }
 })
 
 //Delete collection of pages for a clean slate
+//OPTIMIZED
 app.delete("/pages", (req, res) => {
 
     mongoClient.connect(url, function(err, db) {
@@ -123,19 +104,23 @@ app.delete("/pages", (req, res) => {
           if (delOK) console.log("Collection deleted");
           db.close();
         });
+        pagesDatabase.clear()
       }); 
 })
 
 //Delete collection of pages for a clean slate
+//OPTIMIZED
 app.delete("/pages/:title", (req, res) => {
+
+    const title = req.params.title
 
     mongoClient.connect(url, function(err, db) {
         if (err) throw err;
         const dbo = db.db("mandatoryDB");
-        dbo.collection('pages').deleteOne({ title: req.params.title });
+        dbo.collection('pages').deleteOne({ title: title });
         db.close
-      }); 
-    
+      });
+      pagesDatabase = pagesDatabase.filter(element => element.title !== title)
 })
 
 
